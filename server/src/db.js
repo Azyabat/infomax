3bcfdc3290dc53b58a192c1d55ddf91339f12ec2
Azyabat <azyabat@github.com>
2020-01-21 16:28:04 +0300

    Add server GQL

---
+++ server/src/db.js
@@ -0,0 +1,4 @@
+export let users = [
+    { id: 1, username: "John Doe", email: "john@gmail.com", password:"123" },
+    { id: 2, username: "Jane Doe", email: "jane@gmail.com", password:"qwerty" }
+  ];
